56405a15e77088ed0f25ab6250ef4bb34c73504f
cuijinlong <cuijinlong@hujiang.com>
2019-07-27 13:14:02 +0800

    chap2 补充心得和use case

--- functional-playground/play.js
+++ functional-playground/play.js
@@ -1,7 +1,18 @@
 import forEach from '../lib/es6-functional.js'
 
-var array = [1,2,3]
+var array = [1, 2, 3]
 //print the console
-forEach(array,(data) => console.log(data))
+forEach(array, (data, index) => console.log(index, data))
 //double the contents
-forEach(array,(data) => console.log(2 * data))
+forEach(array, (data) => console.log(2 * data))
+
+// 只打印值为3的元素和位置
+// JS中函数式一等公民
+
+// 抽象的套路1: 把函数作为参数传入
+
+forEach(array, (data, index) => {
+  if (data === 3) {
+    console.log(`${index}: ${data}`)
+  }
+})
